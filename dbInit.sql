ef75a0c5538557892e9227789c0ff2ec7ca8d95d
Nikolaj <supernikolaj@hotmail.com>
2018-04-24 12:24:39 +0200

    database setup

--- dbInit.sql
+++ dbInit.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Synchronization
--- Generated: 2018-04-24 11:15
+-- Generated: 2018-04-24 12:24
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
@@ -9,24 +9,14 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE TABLE IF NOT EXISTS `sem2exam`.`roofs` (
-  `roof_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `price` INT(11) NOT NULL,
-  PRIMARY KEY (`roof_id`),
-  UNIQUE INDEX `roof_id_UNIQUE` (`roof_id` ASC),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
 CREATE TABLE IF NOT EXISTS `sem2exam`.`orders` (
   `order_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
   `address` VARCHAR(100) NOT NULL,
   `zipcode` INT(11) NOT NULL,
-  `city` VARCHAR(45) NOT NULL,
-  `phone` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
+  `city` VARCHAR(100) NOT NULL,
+  `phone` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
   `note` VARCHAR(500) NULL DEFAULT NULL,
   `width` INT(11) NOT NULL,
   `length` INT(11) NOT NULL,
@@ -35,6 +25,7 @@ CREATE TABLE IF NOT EXISTS `sem2exam`.`orders` (
   `shed_width` INT(11) NULL DEFAULT NULL,
   `shed_length` INT(11) NULL DEFAULT NULL,
   `placed` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` VARCHAR(100) NULL DEFAULT 'Behandles',
   PRIMARY KEY (`order_id`, `roof_id`),
   UNIQUE INDEX `order_id_UNIQUE` (`order_id` ASC),
   INDEX `fk_orders_roofs_idx` (`roof_id` ASC),
@@ -45,6 +36,17 @@ CREATE TABLE IF NOT EXISTS `sem2exam`.`orders` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `sem2exam`.`roofs` (
+  `roof_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `price` INT(11) NOT NULL,
+  PRIMARY KEY (`roof_id`),
+  UNIQUE INDEX `roof_id_UNIQUE` (`roof_id` ASC),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_danish_ci;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
