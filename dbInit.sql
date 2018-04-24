abb62f5e9ed1e9722573eb36faf3610e213ac28b
Nikolaj <supernikolaj@hotmail.com>
2018-04-24 11:47:22 +0200

    database setup + rapport

--- dbInit.sql
+++ dbInit.sql
@@ -1,8 +1,23 @@
+-- MySQL Workbench Synchronization
+-- Generated: 2018-04-24 11:15
+-- Model: New Model
+-- Version: 1.0
+-- Project: Name of the project
+-- Author: super
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-ALTER SCHEMA `sem2exam`  DEFAULT COLLATE utf8_general_ci ;
+CREATE TABLE IF NOT EXISTS `sem2exam`.`roofs` (
+  `roof_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `price` INT(11) NOT NULL,
+  PRIMARY KEY (`roof_id`),
+  UNIQUE INDEX `roof_id_UNIQUE` (`roof_id` ASC),
+  UNIQUE INDEX `name_UNIQUE` (`name` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE IF NOT EXISTS `sem2exam`.`orders` (
   `order_id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -15,16 +30,21 @@ CREATE TABLE IF NOT EXISTS `sem2exam`.`orders` (
   `note` VARCHAR(500) NULL DEFAULT NULL,
   `width` INT(11) NOT NULL,
   `length` INT(11) NOT NULL,
-  `roof` VARCHAR(45) NOT NULL,
+  `roof_id` INT(11) NOT NULL,
   `angle` INT(11) NULL DEFAULT NULL,
   `shed_width` INT(11) NULL DEFAULT NULL,
   `shed_length` INT(11) NULL DEFAULT NULL,
   `placed` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`order_id`),
-  UNIQUE INDEX `order_id_UNIQUE` (`order_id` ASC))
+  PRIMARY KEY (`order_id`, `roof_id`),
+  UNIQUE INDEX `order_id_UNIQUE` (`order_id` ASC),
+  INDEX `fk_orders_roofs_idx` (`roof_id` ASC),
+  CONSTRAINT `fk_orders_roofs`
+    FOREIGN KEY (`roof_id`)
+    REFERENCES `sem2exam`.`roofs` (`roof_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
-
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
