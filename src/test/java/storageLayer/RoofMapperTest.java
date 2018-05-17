2d28567662f9c4b81d838ffa5021e87a7a165415
Ejer <Ejer@192.168.1.2>
2018-05-17 10:36:28 +0200

    Setup RoofMapperTest to use test database

--- src/test/java/storageLayer/RoofMapperTest.java
+++ src/test/java/storageLayer/RoofMapperTest.java
@@ -1,8 +1,13 @@
 package storageLayer;
 
+import com.mysql.jdbc.Statement;
 import functionLayer.Roof;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,7 +15,38 @@ import org.junit.Test;
  * @author super
  */
 public class RoofMapperTest {
-    
+
+    private static Connection testConnection;
+    private static String USER = "fogTest";
+    private static String USERPW = "password123";
+    private static String DBNAME = "sem2examTest";
+    private static String HOST = "159.89.9.144";
+
+    @Before
+    public void setUp() {
+        try {
+            // awoid making a new connection for each test
+            if (testConnection == null) {
+                String url = String.format("jdbc:mysql://%s:3306/%s", HOST, DBNAME);
+                Class.forName("com.mysql.jdbc.Driver");
+
+                testConnection = DriverManager.getConnection(url, USER, USERPW);
+                // Make mappers use test 
+                Connector.setConnection(testConnection);
+            }
+            // reset test database
+            try (Statement stmt = (Statement) testConnection.createStatement()) {
+                stmt.execute("drop table if exists roofs");
+                stmt.execute("create table roofs like roofsTest");
+                stmt.execute("insert into roofs select * from roofsTest");
+            }
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            testConnection = null;
+            System.out.println("Could not open connection to database: " + ex.getMessage());
+        }
+    }
+
     public RoofMapperTest() {
     }
 
@@ -37,5 +73,5 @@ public class RoofMapperTest {
         assertEquals(expName, roof.toString());
         assertEquals(expType, roof.getTYPE());
     }
-    
+
 }
