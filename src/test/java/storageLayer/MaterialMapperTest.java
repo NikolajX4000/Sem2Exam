f57101f12f55ed717410bc10bb2413b728cacbb1
Stephan-D-CBA <stephan.mdd@gmail.com>
2018-05-17 10:48:34 +0200

    ToolMapperTest: Test Database

--- src/test/java/storageLayer/MaterialMapperTest.java
+++ src/test/java/storageLayer/MaterialMapperTest.java
@@ -1,8 +1,13 @@
 package storageLayer;
 
+import com.mysql.jdbc.Connection;
 import functionLayer.Material;
+import java.sql.DriverManager;
+import com.mysql.jdbc.*;
+import java.sql.SQLException;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,9 +15,36 @@ import org.junit.Test;
  * @author super
  */
 public class MaterialMapperTest {
+    private static Connection testConnection;
+    private static final String USER = "fogTest";
+    private static final String USERPW = "password123";
+    private static final String DBNAME = "sem2examTest";
+    private static final String HOST = "159.89.9.144";
     
-    public MaterialMapperTest() {
+    @Before
+    public void setUp() {
+    try {        
+        // awoid making a new connection for each test
+        if ( testConnection == null ) {
+            String url = String.format( "jdbc:mysql://%s:3306/%s", HOST, DBNAME );
+            Class.forName( "com.mysql.jdbc.Driver" );
+            
+            testConnection = ( Connection )DriverManager.getConnection( url, USER, USERPW );
+            // Make mappers use test 
+            Connector.setConnection( testConnection );
+        }
+        // reset test database
+        try ( Statement stmt = ( Statement )testConnection.createStatement() ) {
+            stmt.execute( "drop table if exists materials" );
+            stmt.execute( "create table materials like materialsTest" );
+            stmt.execute( "insert into materials select * from materialsTest" );
+        }
+
+    } catch ( ClassNotFoundException | SQLException ex ) {
+        testConnection = null;
+        System.out.println( "Could not open connection to database: " + ex.getMessage() );
     }
+}       
 
     /**
      * Test of getMaterials method, of class MaterialMapper.
