16c2f884cf87052ad5244a94b5ccc4888eb71fa4
Hupra Laptop <test@test.test>
2018-05-18 09:59:57 +0200

    Login virker og gemmer user i session

--- src/main/java/presentationLayer/RenderOrderInspect.java
+++ src/main/java/presentationLayer/RenderOrderInspect.java
@@ -1,7 +1,12 @@
 package presentationLayer;
 
 import functionLayer.CustomException;
+import functionLayer.FlatCarPortList;
 import functionLayer.Order;
+import functionLayer.PartLine;
+import functionLayer.TallCarPortList;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -110,8 +115,35 @@ public class RenderOrderInspect {
         StringBuilder s = new StringBuilder();
         s.append(tabStart("format_list_bulleted", "Stykliste"));
         
-        
-        s.append("<p>Lorem ipsum dolor sit amet.</p>");
+        /////////////////////////////////////////////////////////////////////////////////////
+        s.append("<table><tbody>");
+        s.append("<tr><th>Antal</th><th>Størrelse</th><th>Navn</th></tr>");
+        try {
+
+        //ArrayList
+        List<PartLine> list = new ArrayList<>();
+        if(o.isFlat()){
+            FlatCarPortList cpl = new FlatCarPortList(o);
+            list = cpl.getParts();
+        }else
+        {
+            TallCarPortList tcpl = new TallCarPortList(o);
+            list = tcpl.getParts();
+        }
+            for(PartLine pl : list){
+                s.append("<tr>");
+
+                s.append("<td>").append(pl.getAmount()).append("</td>");
+                s.append("<td>").append(pl.getSize()).append("</td>");
+                s.append("<td>").append(pl.getMaterial().getName()).append("</td>");
+
+                s.append("</tr>");
+            }
+        } catch (Exception e) {
+            s.append(e);
+        }
+        s.append("</tbody></table>");
+        /////////////////////////////////////////////////////////////////////////////////////
         
         
         s.append(tabEnd());
