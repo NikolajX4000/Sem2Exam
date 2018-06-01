908868594442cc9c69400a0826d66c4af1f0c0e5
Hupra <36619416+Hupra@users.noreply.github.com>
2018-06-01 10:38:13 +0200

    Java *Drawing* Ducks

--- src/main/java/logicLayer/Roof.java
+++ src/main/java/logicLayer/Roof.java
@@ -10,24 +10,46 @@ public class Roof {
     private final String NAME;
     private final int TYPE;
 
+    /**
+     *
+     * @param id
+     * @param name
+     * @param type
+     */
     public Roof(int id, String name, int type) {
         this.ID = id;
         this.NAME = name;
         this.TYPE = type;
     }
 
+    /**
+     *
+     * @return roof ID
+     */
     public int getID() {
         return ID;
     }
 
+    /**
+     *
+     * @return roof NAME
+     */
     public String getNAME() {
         return NAME;
     }
 
+    /**
+     *
+     * @return roof TYPE
+     */
     public int getTYPE() {
         return TYPE;
     }
 
+    /**
+     *
+     * @return name of the roof
+     */
     @Override
     public String toString() {
         return NAME;
